538c19153e2dbd43c2ad9f4e87ae587a35ce7e0d
siddapichau <161685092+siddapichau@users.noreply.github.com>
2026-06-29 23:31:26 -0300

    Delete banco.js

--- banco.js
+++
@@ -1,5 +0,0 @@
-var Banco = {
-  salvar:function(c,v){try{localStorage.setItem("rds_"+c,JSON.stringify(v))}catch(e){}},
-  pegar:function(c,d){try{var x=localStorage.getItem("rds_"+c);return x?JSON.parse(x):d}catch(e){return d}},
-  remover:function(c){localStorage.removeItem("rds_"+c)}
-};
